2ba618ea5bf1c3f56b50fc1f1ea2a8f410881991
Aleksandrov Dmitriy <goodmobiledevices@gmail.com>
2022-10-23 18:50:00 +0000

    src && docs: added tftp entities
    Signed-off-by: Aleksandrov Dmitriy <goodmobiledevices@gmail.com>

---
+++ src/domain/TFTPConfig.go
@@ -0,0 +1,12 @@
+package domain
+
+//TFTPConfig TFTP config entity
+type TFTPConfig struct {
+	EntityUUID
+	//Address TFTP server IP address
+	Address string
+	//Port TFTP server port
+	Port string
+	//Enabled TFTP server startup status
+	Enabled bool
+}
